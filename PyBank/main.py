a38e392d89fddcf0cb0db7953b78371140d2c2d6
AhldenBrough <50213731+AhldenBrough@users.noreply.github.com>
2023-09-20 16:27:23 -0700

    Added functionality for tracking net total of Profits/Losses

---
+++ PyBank/main.py
@@ -0,0 +1,25 @@
+import csv
+import os
+
+#build path for the csv using os module
+csvpath = os.path.join("Resources", "budget_data.csv")
+
+#initialize total
+total = 0
+
+#initialize variables to track greatest increase and decrease as we step through the csv
+greatest_increase = 0
+greatest_decrease = 0
+inc_month = ""
+dec_month = ""
+
+#open csv file
+with open(csvpath, encoding='UTF-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    
+    #skip header
+    next(csvreader, None)
+
+    #step through month by month (row by row)
+    for month in csvreader:
+        total += int(month[1])
