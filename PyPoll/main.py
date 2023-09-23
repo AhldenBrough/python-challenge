0d8531de6ca34ed29b41530f731263ec76622412
AhldenBrough <50213731+AhldenBrough@users.noreply.github.com>
2023-09-22 17:29:03 -0700

    corrected vote count for final candidate, added output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,27 +1,11 @@
-# In this Challenge, you are tasked with helping a small, rural town modernize its vote-counting process.
-
-# You will be given a set of poll data called election_data.csv. The dataset is composed of three columns: "Voter ID", "County", and "Candidate". Your task is to create a Python script that analyzes the votes and calculates each of the following values:
-
-# The total number of votes cast
-
-# A complete list of candidates who received votes
-
-# The percentage of votes each candidate won
-
-# The total number of votes each candidate won
-
-# The winner of the election based on popular vote
-
-
 import os
 import csv
 
 # build path for the csv using os module
 csvpath = os.path.join("Resources", "election_data.csv")
 
-prev_vote_candidate = ""
+#initialize dictionary to keep track of candidates and votes, key value pairs will be: candidate, vote count
 votes_by_candidate = {}
-total_votes = 0
 
 # open csv file
 with open(csvpath, encoding='UTF-8') as csvfile:
@@ -33,30 +17,37 @@ with open(csvpath, encoding='UTF-8') as csvfile:
     # for each vote (keeping track of vote count with enumerate)
     for count, vote in enumerate(csvreader, start=1):
 
-        #if current vote is for a different candidate than the previous
-        if vote[2] != prev_vote_candidate and prev_vote_candidate != "":
+        #if the candidate isn't in the dictionary of candidates
+        if vote[2] not in votes_by_candidate:
 
-            #if we weren't tracking that candidate yet
-            if prev_vote_candidate not in votes_by_candidate:
+            #set vote count to 0, we will incrememnt outside of if block
+            votes_by_candidate[vote[2]] = 0
 
-                #add the candidate and their vote count to the dictionary
-                votes_by_candidate[prev_vote_candidate] = total_votes
+        #increment vote count for the selected candidate
+        votes_by_candidate[vote[2]] += 1
 
-                #reset total_votes as we are moving on to another candidate
-                total_votes = 0
-            
-            #if we were already tracking that candidate
-            else:
-                
-                #add total_votes to their vote count
-                votes_by_candidate[prev_vote_candidate] += total_votes
+# define path for output
+output_path = os.path.join("analysis", "analysis.txt")
 
-                #reset total_votes as we are moving on to another candidate
-                total_votes = 0
+# open output file for writing
+f_output = open(output_path, "w")
 
-        #increment total votes, whether or not we moved on to a new candidate we just counted a vote
-        total_votes+=1
-        prev_vote_candidate = vote[2]
+# write output
+f_output.write("Election Results\n")
+f_output.write("-------------------------\n")
+f_output.write(f"Total Votes: {count}\n")
+f_output.write("-------------------------\n")
 
-print(votes_by_candidate)
-print(count)
+# for each key value pair in our dictionary, calculate the percentage of the total vote
+for candidate, votes in votes_by_candidate.items() :
+    f_output.write(f"{candidate}: {round( ((votes/count) * 100), 3 )}% ({votes})\n")
+f_output.write("-------------------------\n")
+
+# Citation: comment from Atcold https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+f_output.write(f"Winner: {max(votes_by_candidate, key=lambda key: votes_by_candidate[key])}\n")
+f_output.write("-------------------------\n")
+
+# print output file to stdout
+# Citation David Alber https://stackoverflow.com/questions/8084260/how-to-print-a-file-to-stdout
+with open(output_path, 'r') as f_output:
+    print(f_output.read())
